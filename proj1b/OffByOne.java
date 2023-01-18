8cd9986673a7f66887a57b08329e102af917bb7d
EatingBrownie <117447369+EatingBrownie@users.noreply.github.com>
2023-01-18 20:21:45 +0800

    renew offbyone

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -3,15 +3,18 @@ public class OffByOne implements CharacterComparator {
 
     @Override
     public boolean equalChars(char x, char y) {
-        if ((x >= 66 && x <= 89) || (x >= 98 && x <= 121)) {
+        if ((x >= 66 && x <= 89)) {
             return ((x + 1) == y
                     || (x - 1) == y
-                    || (x + 31) == y
-                    || (x - 31) == y);
+                    || (x + 31) == y);
         } else if (x == 65 || x == 97) {
             return (y == 66 ||  y == 98 );
         } else if (x == 90 || x == 122) {
             return (y == 121 || y == 89);
+        } else if ((x >= 98 && x <= 121)) {
+            return ((x + 1) == y
+                    || (x - 1) == y
+                    || (x - 31) == y);
         }
 
         return (((x - 1) == y) || ((x + 1) == y));
